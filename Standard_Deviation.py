a6a5c3da3d3afd7810348d637a43e83b1831a9d2
Urvashi tiwari <priyatiwari0425@gmail.com>
2025-05-29 13:48:24 +0530

    Add files via upload

---
+++ Standard_Deviation.py
@@ -0,0 +1,8 @@
+import numpy as np 
+numbers=[10,20,30,40,50]
+arr=np.array(numbers)
+mean_value=np.mean(arr)
+std_deviation=np.std(arr)
+print("Mean:",mean_value)
+print("Standard Deviation:",std_deviation)
+# Output: Mean:30.0, Standard Deviation:14.142135623730951
